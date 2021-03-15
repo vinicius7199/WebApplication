d952022c23bed4409d2bb9fcb78218da9b0f127c
vinicius7199 <viniciusdeandradefernandes5@gmail.com>
2021-03-15 16:01:06 -0300

    Update

--- Frontend/WebApplication-App/src/app/app-routing.module.ts
+++ Frontend/WebApplication-App/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventosComponent } from './components/eventos/eventos.component';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+
+const routes: Routes = [
+  { path: 'eventos', component: EventosComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'palestrantes', component: PalestrantesComponent},
+  { path: 'contatos', component: ContatosComponent},
+  { path: 'perfil', component: PerfilComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/WebApplication-App/src/app/app.module.ts
+++ Frontend/WebApplication-App/src/app/app.module.ts
@@ -13,9 +13,13 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { NavComponent } from './nav/nav.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
+import { TituloComponent } from './shared/titulo/titulo.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { NavComponent } from './shared/titulo/nav/nav.component';
 
 import { EventoService } from './services/evento.service';
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
@@ -26,6 +30,10 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
     EventosComponent,
     PalestrantesComponent,
     NavComponent,
+    TituloComponent,
+    ContatosComponent,
+    DashboardComponent,
+    PerfilComponent,
     DateTimeFormatPipe
    ],
   imports: [
